0ca2a48255c574a89018afd36e41d838b651f138
aaazhouaa <92150833+aaazhouaa@users.noreply.github.com>
2026-09-19 17:59:54 +0000

    Bump version to 0.18.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
         applicationId = "dev.anilbeesetti.nextplayer"
-        versionCode = 74
-        versionName = "0.18.0"
+        versionCode = 75
+        versionName = "0.18.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
